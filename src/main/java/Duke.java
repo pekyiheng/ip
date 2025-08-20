1cf91a8ae487e3dcf158d6bbec53f3ea85c9d689
pekyiheng <105923054+pekyiheng@users.noreply.github.com>
2025-08-20 20:49:59 +0800

    Add greetings to Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+package main.java;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,12 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        String lineBreaks = "____________________________________________________________";
         System.out.println("Hello from\n" + logo);
+        System.out.println(lineBreaks);
+        System.out.println("Hello! I'm Duke\nHow may I help you?");
+        System.out.println(lineBreaks);
+        System.out.println("Bye. Hope to see you again!\n");
+        System.out.println(lineBreaks);
     }
 }
