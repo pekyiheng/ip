a68c1a7c71627be734edf1cdac246cff55a66647
pekyiheng <105923054+pekyiheng@users.noreply.github.com>
2025-08-20 21:35:49 +0800

    Rename chatbot to Hamlet

---
+++ src/main/java/Hamlet.java
@@ -0,0 +1,12 @@
+package main.java;
+
+public class Hamlet {
+    public static void main(String[] args) {
+        String lineBreaks = "____________________________________________________________";
+        System.out.println(lineBreaks);
+        System.out.println("Hello! I'm Hamlet\nHow may I help you?");
+        System.out.println(lineBreaks);
+        System.out.println("Bye. Hope to see you again!\n");
+        System.out.println(lineBreaks);
+    }
+}
