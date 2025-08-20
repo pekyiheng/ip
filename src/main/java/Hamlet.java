ba4968e6c2861721b11f44d0ec8cad6f73d24991
pekyiheng <105923054+pekyiheng@users.noreply.github.com>
2025-08-20 23:30:37 +0800

    Allow basic inputs for chatbot
    Asking "what is your name" prompts chatbot to tell user its name

--- src/main/java/Hamlet.java
+++ src/main/java/Hamlet.java
@@ -1,10 +1,23 @@
 package main.java;
+import java.util.Scanner;
 
 public class Hamlet {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String lineBreaks = "____________________________________________________________";
         System.out.println(lineBreaks);
         System.out.println("Hello! I'm Hamlet\nHow may I help you?");
+        String input = scanner.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println(lineBreaks);
+            if (input.equals("what is your name")) {
+                System.out.println("\t" + "I am Hamlet!");
+            } else {
+                System.out.println("\t" + input);
+            }
+            System.out.println(lineBreaks);
+            input = scanner.nextLine();
+        }
         System.out.println(lineBreaks);
         System.out.println("Bye. Hope to see you again!\n");
         System.out.println(lineBreaks);
