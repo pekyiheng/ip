6bcfbbf79d70ac791566dd4a60937796f7d3a7a2
pekyiheng <105923054+pekyiheng@users.noreply.github.com>
2025-08-21 17:41:43 +0800

    Add tests for errors

--- src/main/java/HamletException.java
+++ src/main/java/HamletException.java
@@ -5,7 +5,7 @@ public class HamletException extends Exception{
     @Override
     public String toString() {
         return "I'm sorry, this command is not recognized. Accepted commands:\n- what is your name\n- bye\n- list\n" +
-                "- mark [index]\n- unmark index\n- todo [desc]\n- deadline [desc] /by [when]\n" +
+                "- mark [index]\n- unmark [index]\n- todo [desc]\n- deadline [desc] /by [when]\n" +
                 "- event [desc] /from [when] /to [when]\n";
     }
 }
