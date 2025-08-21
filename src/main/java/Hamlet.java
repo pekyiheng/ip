437caadf0661921da8e2eceb5bd21d67259c0f11
pekyiheng <105923054+pekyiheng@users.noreply.github.com>
2025-08-21 13:25:26 +0800

    Add ability to store text
    Text displayed when requested by user

--- src/main/java/Hamlet.java
+++ src/main/java/Hamlet.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 public class Hamlet {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] inputs = new String[100];
+        int count = 0;
         String lineBreaks = "____________________________________________________________";
         System.out.println(lineBreaks);
         System.out.println("Hello! I'm Hamlet\nHow may I help you?");
@@ -12,8 +14,14 @@ public class Hamlet {
             System.out.println(lineBreaks);
             if (input.equals("what is your name")) {
                 System.out.println("\t" + "I am Hamlet!");
+            } else if (input.equals("list")) {
+                for (int i = 0; i < count; i++) {
+                    System.out.printf("\t%d.%s%n", i+1, inputs[i]);
+                }
             } else {
-                System.out.println("\t" + input);
+                System.out.println("\tadded: " + input);
+                inputs[count] = input;
+                count++;
             }
             System.out.println(lineBreaks);
             input = scanner.nextLine();
