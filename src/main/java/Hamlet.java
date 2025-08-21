9bc459af46b699d5752af3b5231c3e282c0098bb
pekyiheng <105923054+pekyiheng@users.noreply.github.com>
2025-08-21 13:55:45 +0800

    Add ability to mark tasks as done
    Add a new class called Task

--- src/main/java/Hamlet.java
+++ src/main/java/Hamlet.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Hamlet {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] inputs = new String[100];
+        Task[] inputs = new Task[100];
         int count = 0;
         String lineBreaks = "____________________________________________________________";
         System.out.println(lineBreaks);
@@ -16,11 +16,25 @@ public class Hamlet {
                 System.out.println("\t" + "I am Hamlet!");
             } else if (input.equals("list")) {
                 for (int i = 0; i < count; i++) {
-                    System.out.printf("\t%d.%s%n", i+1, inputs[i]);
+                    Task curTask = inputs[i];
+                    System.out.printf("\t%d.[%s] %s%n", i+1, curTask.getStatusIcon(), curTask.description);
                 }
-            } else {
+            } else if (input.matches("mark \\d+")) {
+                int indexToEdit = Integer.parseInt(input.replaceAll("[^\\d]", ""));
+                Task curTask = inputs[indexToEdit - 1]; //inputs is 0-indexed
+                curTask.markAsDone();
+                System.out.print("Nice! I've marked this task as done:\n");
+                System.out.printf("[%s] %s%n", curTask.getStatusIcon(), curTask.description);
+            } else if (input.matches("unmark \\d+")) {
+                int indexToEdit = Integer.parseInt(input.replaceAll("[^\\d]", ""));
+                Task curTask = inputs[indexToEdit - 1]; //inputs is 0-indexed
+                curTask.markAsUndone();
+                System.out.print("Ok, I've marked this task as not done yet:\n");
+                System.out.printf("[%s] %s%n", curTask.getStatusIcon(), curTask.description);
+            }
+            else {
                 System.out.println("\tadded: " + input);
-                inputs[count] = input;
+                inputs[count] = new Task(input);
                 count++;
             }
             System.out.println(lineBreaks);
