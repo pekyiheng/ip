aef01a021ccc7b1f44b8d83bf5b069414a769eb6
pekyiheng <105923054+pekyiheng@users.noreply.github.com>
2025-09-01 20:26:32 +0800

    Add JavaDoc comments for Parser.java

--- src/main/java/hamlet/parser/Parser.java
+++ src/main/java/hamlet/parser/Parser.java
@@ -14,13 +14,33 @@ import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * Parses user input and converts it into a strucutred format that the program can handle
+ * <p>
+ *     This class provides static methods to interpret user commands, extract relevant information, and creates
+ *     the correct Task objects
+ * </p>
+ */
 public class Parser {
     static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
+    /**
+     *Extracts and returns a 0-indexed integer from a user's input. This method is used to identify which task
+     * to perform a task on.
+     *
+     * @param input The user's input containing a number
+     * @return 0-indexed integer based on input
+     */
     public static int getIndexToEdit(String input) {
         return Integer.parseInt(input.replaceAll("[^\\d]", "")) - 1; //inputs is 0-indexed
     }
 
+    /** Matches user input to the todo command and creates a Todo task
+     *
+     * @param input the user's input command
+     * @return A new Todo object
+     * @throws TodoException If input does not match todo command format
+     */
     public static Task matchInputToDo(String input) throws TodoException {
         Pattern pattern = Pattern.compile("todo (.+)");
         Matcher matcher = pattern.matcher(input);
@@ -33,6 +53,12 @@ public class Parser {
         }
     }
 
+    /** Matches user input to the deadline command and creates a Deadline task
+     *
+     * @param input the user's input command
+     * @return A new Deadline object
+     * @throws DeadlineException If input does not match deadline command format
+     */
     public static Task matchInputDeadline(String input) throws DeadlineException {
         Pattern pattern = Pattern.compile("deadline (.*) /by (.*)");
         Matcher matcher = pattern.matcher(input);
@@ -47,6 +73,12 @@ public class Parser {
         }
     }
 
+    /** Matches user input to the event command and creates a Event task
+     *
+     * @param input the user's input command
+     * @return A new Event object
+     * @throws EventException If input does not match event command format
+     */
     public static Task matchInputEvent(String input) throws EventException {
         Pattern pattern = Pattern.compile("event (.+) /from (.+) /to (.+)");
         Matcher matcher = pattern.matcher(input);
@@ -62,6 +94,13 @@ public class Parser {
         }
     }
 
+    /** Matches user input to the happening command and finds deadlines and events that occur on a specific date
+     *
+     * @param input The user's input command
+     * @param inputs The list of all tasks
+     * @param count The current number of tasks in the list
+     * @return An array of 2 stringsc containing a list of deadlines and events
+     */
     public static String[] matchHappenings(String input, ArrayList<Task> inputs, int count) {
         Pattern pattern = Pattern.compile("happening /on (.+)");
         Matcher matcher = pattern.matcher(input);
@@ -88,6 +127,12 @@ public class Parser {
         return returnArray;
     }
 
+    /**
+     * Converts an ArrayList of tasks into a single string separated by commas and newlines.
+     *
+     * @param inputs The ArrayList of tasks to convert
+     * @return A string representation of all tasks to be stored in text file
+     */
     public static String convertArrToString(ArrayList<Task> inputs) {
         String finalString = "";
         for (Task task : inputs) {
